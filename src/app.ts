248eab24d2a7943582de052858aa1d87ae9ed6f3
whiterqbbit <wedd13@gmail.com>
2023-06-15 11:58:14 +0200

    feat(error): class for status code transfer

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,11 @@ import type { Request, Response, NextFunction } from 'express'
 import Router from './router'
 import morgan from 'morgan'
 import helmet from 'helmet'
+import { HttpError } from './utils/HttpError'
 
 const app = express()
 
-app.use(express.json())
+app.use(express.text())
   .use(morgan('tiny'))
   .use(helmet())
 
@@ -18,8 +19,11 @@ app.use(Router)
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((err: Error, req: Request, res: Response, next: NextFunction ) => {
+  if (err instanceof HttpError) res.status(err.statusCode)
+  else res.status(500)
+  
   console.error('ERROR:', err.message)
-  res.status(500).json({ error: err.message })
+  res.json({ message: err.message })
 })
 
 export default app
--- src/entities/justify/justify.controller.ts
+++ src/entities/justify/justify.controller.ts
@@ -1,13 +1,26 @@
 import type { NextFunction, Request, Response } from 'express'
+import config from '../../../config'
+import { HttpError } from '../../utils/HttpError'
 
 export class JustifyController {
   static async justify(req: Request, res: Response, next: NextFunction) {
     try {
-      const input = req.body.text
-      if (typeof input !== 'string') {
-        throw new Error('Input must be a string')
+      if (req.headers['content-type'] !== 'text/plain') {
+        throw new HttpError('Invalid Content-Type. Expected text/plain.', 415)
       }
-      res.json({ message: `Let us justify, ${input} !` })
+      const textInput = req.body
+      // counts the words
+      const wordCount = textInput.split(' ').length
+      // adds them to the user's word count and throws an error 402 if they exceed the limit
+      if (wordCount > config.DAILY_LIMIT) {
+        throw new HttpError('You have exceeded your word count limit', 402)
+      }
+
+      console.log(wordCount)
+
+      const splitText = textInput.split(' ').join('\n')
+
+      res.status(200).type('text/plain').send(splitText)
     } catch (error) {
       next(error)
     }
